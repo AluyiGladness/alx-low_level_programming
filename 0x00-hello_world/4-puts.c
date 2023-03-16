edde93133ec47e2707ffe9375585846270e6784a
AluyiGladness <gangliahemit@gmail.com>
2023-03-16 21:23:36 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.>
+/**
+ * main - start point
+ *
+ * Return: no problems
+ */
+int main(void)
+{   puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
