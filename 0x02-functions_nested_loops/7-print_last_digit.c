25c92681a780889006721be6c966c14c0262edb3
AluyiGladness <gangliahemit@gmail.com>
2023-03-22 10:21:54 +0100

    updated the 7-print_last_digits.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit - main function
+ *
+ * @n: number to print its last digit
+ *
+ * Return: returns last digit
+ *
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
