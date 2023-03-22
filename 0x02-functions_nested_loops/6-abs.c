735d0d301f0a44ab3c2906f9670a2efc1ba1e730
AluyiGladness <gangliahemit@gmail.com>
2023-03-22 09:31:28 +0100

    updated the 6-abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - gives the absolute value
+ *
+ * @n: integer to make absolute
+ *
+ * Return: int or int multiplied by negative
+ *
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (n * -1);
+}
