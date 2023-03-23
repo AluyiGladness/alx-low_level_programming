64c012b8d5cc1b9341bbedfe047875feb3408d86
AluyiGladness <gangliahemit@gmail.com>
2023-03-23 10:05:28 +0100

    fixed the 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,12 @@
  * Return: always Success
  *
  */
+
 void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 98)
+	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
@@ -23,22 +24,17 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-
 		}
-	}
-		else
+	} else
+	{
+		for (i = n; i <= 98; i++)
 		{
-			for (i = n; i <= 98; i++)
+			printf("%d", i);
+			if (i != 98)
 			{
-				printf("%d", i);
-				if (i != 98)
-				{
-					printf(", ");
-				}
+				printf(", ");
 			}
 		}
 	}
 	printf("\n");
 }
-
-
