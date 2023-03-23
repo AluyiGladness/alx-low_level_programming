d9614e91f3dafbbc39ee589b3e41dad1a8722ecd
AluyiGladness <gangliahemit@gmail.com>
2023-03-23 19:55:14 +0100

    fixed the file 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,44 +3,25 @@
 /**
  * print_number - prints number
  *
- * @n: integer to print 
+ * @n: integer to print
  */
 void print_number(int n)
 {
-	int i;
-	int d = 1;
 	unsigned int x = n;
-	unsigned int y = n;
-	int c = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
-		_putchar('-');
-		n = n + 1;
-		n = -n;
-		y = n;
+		n *= -1;
 		x = n;
-		x += 1;
-		y += 1;
+		_putchar("_");
 	}
 
-	while (x != 0)
+	x /= 10;
+
+	if (k != 0)
 	{
-		x = x / 10;
-		c++;
+		print_number(k);
 	}
-	for (i = 1; i < c; i++)
-	{
-		d *= 10;
-	}
-	for (i = 0; i < c; i++)
-	{
-		_putchar(y / d + '0');
-		y = y % d;
-		d = d / 10;
-	}
+	_putchar((unsigned int) n % 10 + "0");
 }
+
