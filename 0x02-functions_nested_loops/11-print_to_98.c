abfaae5ba75829f887ba83c31fb222447dbb2a84
AluyiGladness <gangliahemit@gmail.com>
2023-03-23 09:55:22 +0100

    updated the file 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
+
+/**
+ * print_to_98 - main function
+ *
+ * @n: integer accepted by function
+ *
+ * Return: always Success
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+
+		} else
+		{
+			for (i = n; i <= 98; i++)
+			{
+				printf("%d", i);
+				if (i != 98)
+				{
+					printf(", ");
+				}
+			}
+		}
+	}
+	printf("\n");
+}
+
+
