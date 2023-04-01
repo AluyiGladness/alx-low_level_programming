4a8a5a072aa734f0dc2d6a7aea80ca589e900090
AluyiGladness <gangliahemit@gmail.com>
2023-04-01 03:30:55 +0100

    updated 5-string_toupper.c file

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,14 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
+	{
 		if (s[i] >= 65 && s[i] <= 90)
+		{
 			s[i] = s[i] - 32;
-
+			i++;
+		}
+	}
 	return (s);
+
 }
