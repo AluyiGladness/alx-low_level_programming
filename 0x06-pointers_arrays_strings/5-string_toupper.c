935504abfab8d8a587091050dac168ce0eb3eb4a
AluyiGladness <gangliahemit@gmail.com>
2023-04-01 02:46:31 +0100

    git fixed 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper -converts a stting in lower format to upper
+ * @s: character to make upper
+ * Return: return value of upper
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 65 && s[i] <= 90)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+}
