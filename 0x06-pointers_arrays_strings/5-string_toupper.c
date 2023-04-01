1dbced41e50e7d717e5024d0446a461a27e0d2d9
AluyiGladness <gangliahemit@gmail.com>
2023-04-01 03:21:44 +0100

    updated 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_toupper -converts a stting in lower format to upper
+ * string_toupper - converts uppers to lowers
  * @s: character to make upper
  * Return: return value of upper
  */
@@ -8,13 +8,9 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != 0; i++)
-	{
+	for (i = 0; s[i] != '\0'; i++)
 		if (s[i] >= 65 && s[i] <= 90)
-		{
 			s[i] = s[i] - 32;
-		}
-	}
-	return (s);
 
+	return (s);
 }
