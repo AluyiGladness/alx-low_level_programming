918dcc0d93c8e382e5db2473d10e157d09f8f7c3
AluyiGladness <gangliahemit@gmail.com>
2023-04-01 03:42:09 +0100

    finally fixed 5-string_toupper.c file

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 65 && s[i] <= 90)
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
-			i++;
 		}
 	}
 	return (s);
-
 }
