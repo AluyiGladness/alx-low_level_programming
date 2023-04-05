56f4f295268579706f8b53f0880aa67b74b565e2
AluyiGladness <gangliahemit@gmail.com>
2023-04-05 03:57:23 +0100

    updated the file 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the array to print
+ * Return: nothing for now
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j;
+	int k;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[j][k]);
+		}
+		_putchar ('\0');
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _strcmp(char *s1, char *s2);
-
+void print_chessboard(char (*a)[8]);
 #endif
