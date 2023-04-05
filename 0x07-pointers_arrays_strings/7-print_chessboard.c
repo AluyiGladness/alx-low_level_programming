891207ab8de142ff2d1faf18f02346a285915919
AluyiGladness <gangliahemit@gmail.com>
2023-04-05 04:01:26 +0100

    fixed 7-print_chessboard.c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int j;
+	int j = 0;
 	int k;
 
-	for (j = 0; j < 8; j++)
+	for (; j < 8; j++)
 	{
 		for (k = 0; k < 8; k++)
 		{
 			_putchar(a[j][k]);
 		}
-		_putchar ('\0');
+		_putchar ('\n');
 	}
 }
