4205d9fd0e8f13a97c0e79e825545c3d01d31ebc
AluyiGladness <gangliahemit@gmail.com>
2023-04-07 02:03:28 +0100

    update 4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints the value of x raised to the power of y.
+ * @x: integer 1
+ * @y: integer 2
+ *
+ * Return: returns the pow or -1 error
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+
+}
