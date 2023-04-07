40c02d18d3d32e7621e87d836572ddee5b6b08d7
AluyiGladness <gangliahemit@gmail.com>
2023-04-07 02:57:02 +0100

    updated 6_is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number - identify prime numbers
+ * @n: number to examine
+ * Return: returns 0 0r 1
+ */
+int is_prime_number(int n)
+{
+	if (n >= 2)
+	{
+		if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 || n < 0)
+		{
+			return (0);
+		}
+		return (1);
+	}
+
