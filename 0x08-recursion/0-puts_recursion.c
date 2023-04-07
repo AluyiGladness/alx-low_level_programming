0121d2c9de161274d00c32ca87c43cdabf1cadfc
AluyiGladness <gangliahemit@gmail.com>
2023-04-07 01:10:44 +0100

    fixed 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: parameter required by the function, this is recursed through
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+	{
+		_putchar('\n');
+	}
+	else
+		_putchar(*s);
+		_puts_recursion(++s);
+}
