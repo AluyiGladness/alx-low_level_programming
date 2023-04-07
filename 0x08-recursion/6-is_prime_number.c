f4f0d715be48822f1f669f4ffdf7e487bf079515
AluyiGladness <gangliahemit@gmail.com>
2023-04-07 03:05:23 +0100

    updated 6_is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,12 +6,12 @@
  */
 int is_prime_number(int n)
 {
-	if (n >= 2)
+	if (n > 2)
 	{
-		if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 || n < 0)
+		if ((n % 2 != 0 && n % 3 != 0) && (n % 5 != 0 || n < 0))
 		{
 			return (0);
 		}
 		return (1);
 	}
-
+}
