fe96e61ae4d2a6a05829131b46b93c8ffb03da6a
AluyiGladness <gangliahemit@gmail.com>
2023-04-08 10:36:27 +0100

    update 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of argument a user pass into it
+ * @argc: argument count of programme
+ * @argv: array of arguments ( array of pointers)
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+
+}
