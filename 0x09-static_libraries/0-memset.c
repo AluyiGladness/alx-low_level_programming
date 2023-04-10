194c4d9d4674d29fed6c597faee3ca09b3d8380a
AluyiGladness <gangliahemit@gmail.com>
2023-04-10 20:18:56 +0100

    updated some files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: points to memory area that will be changed
+ * @b: this is what we are setting the memory to
+ * @n: n number of times to set
+ * Return: will return the value of s of type char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+
+}
