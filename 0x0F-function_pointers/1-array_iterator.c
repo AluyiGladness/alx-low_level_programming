15e91afcd2d351436ae880101365b5e1aadbbdb3
AluyiGladness <gangliahemit@gmail.com>
2023-04-20 01:59:14 +0100

    fixed 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,16 +4,13 @@
  * array_iterator - iterates through array
  * @array: array to iterate through
  * @size: array size
- * @action: function to carry out
+ * @action: function pointer
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || action == NULL)
-		return ();
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+	if (array && action)
+		for  (i = 0; i < size; i++)
+			action(array[i]);
 }
