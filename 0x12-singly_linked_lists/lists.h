4c41c17d0502ca50eb3a3ac26ad0261b69c3b327
AluyiGladness <gangliahemit@gmail.com>
2023-04-28 04:33:45 +0100

    final commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,9 +7,9 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlength(( const char *str);
 #include <stddef.h>
-
-
-
+#include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
