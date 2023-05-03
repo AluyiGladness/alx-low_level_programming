3f7eda7fcac5354f4e4d10622586066b4ad0ccc1
AluyiGladness <gangliahemit@gmail.com>
2023-05-03 02:43:10 +0100

    updated 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stddef.h>
+
+
 /**
  * print_listint - Prints out all the elements of the listint_t list.
  * @h: a Pointer that points to the head of the list.
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,100 +1,31 @@
 #include "lists.h"
-
 /**
- * count_nodes_before_loop - nodes to print
- * @head: pointer to head node of linked list
- * Return: number of unique nodes in list till a loop
- */
-int count_nodes_before_loop(const listint_t *head)
-{
-	int count = 0;
-	const listint_t *tort, *hare;
-
-	tort = hare = head;
-
-	while (tort != NULL && hare != NULL)
-	{
-		tort = tort->next;
-		hare = hare->next->next;
-		count++;
-
-		if (tort == hare)
-		{
-			tort = head;
-			while (tort != hare)
-			{
-				tort = tort->next;
-				hare = hare->next;
-				count++;
-			}
-			return (count);
-		}
-	}
-	return (0);
-}
-
-/**
- * is_loop_present - searches for a loop in linked list
- * @head: pointer to head node of linked list
- * Return: 0 if no loop, 1 if loop
- */
-
-int is_loop_present(const listint_t *head)
-{
-	const listint_t *tort, *hare;
-
-	tort = hare = head;
-
-	while (tort != NULL && hare != NULL)
-	{
-		tort = tort->next;
-		hare = hare->next->next;
-
-		if (tort == hare)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * print_listint_safe - prints linked list
- * @head: pointer to head node of list
- * Return: node count in list, exit(98) if it fails
+ * print_listint_safe - prints a linked list
+ * @head: pointer to the head node
+ * Return: number of nodes in list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	int loop_present, counter = 0;
-	size_t num_nodes = 0;
-	const listint_t *temporary;
+	const listint_t *present;
+	size_t counter = 0;
 
-	if (head == NULL)
-		exit(98);
-
-	loop_present = is_loop_present(head);
-
-	if (loop_present == 1)
+	while (head && head > head->next)
 	{
-		counter = count_nodes_before_loop(head);
-		for (loop_present = 0; loop_present < counter; loop_present++)
-		{
-			printf("[%p] %d\n", (void *)temporary, temporary->n);
-			num_nodes += 1;
-			temporary = temporary->next;
-		}
-	}
-	else if (loop_present == 0)
-	{
-		temporary = head;
-		while (temporary != NULL)
-		{
-			printf("[%p] %d\n", (void *)temporary, temporary->n);
-			num_nodes += 1;
-			temporary = temporary->next;
-		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		counter++;
+		present = head;
+		head = head->next;
+		if (present <= head)
+			exit(98);
 	}
 
-	return (num_nodes);
+	if (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		counter++;
+	}
 
+	return (counter);
 }
 
