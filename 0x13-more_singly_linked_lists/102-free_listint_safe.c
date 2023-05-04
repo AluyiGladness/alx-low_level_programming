190991482d62ea7ff65be29a3e9617bc1486a618
AluyiGladness <gangliahemit@gmail.com>
2023-05-04 01:41:44 +0100

    update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,27 +1,71 @@
 #include "lists.h"
-/**
- * free_listint_safe - frees a linked list
- * @head; double pointer to head node
- * Return: size of list that is freed
- *
- */
-size_t free_listint_safe(listint_t **head)
-{
-	size_t counter = 0;
-	listint_t *temporary;
 
-	while (*head)
+/**
+ * examine_listint_loop -  finds a loop in a linked list
+ * @head: start of the linked list to search
+ * Return: Return the address of node where loop starts
+ * or NULL if loop is absent
+ */
+listint_t *examine_listint_loop(listint_t *head)
+{
+	listint_t *fast, *last;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (last = head->next; last != NULL; last = last->next)
 	{
-		counter++;
-		temporary = *head;
-		*head = (*head)->next;
-		free(temporary);
-		if (temporary <= *head)
-			break;
+		if (last == last->next)
+			return (last);
+
+		for (fast = head; fast != last; fast = fast->next)
+
+			if (fast == last->next)
+				return (last->next);
 	}
 
-	*head = NULL;
+	return (NULL);
 
-	return (counter);
 }
 
+/**
+ * free_listint_safe - frees a linked list
+ * @h: double pointer to head node of a linked list
+ * Return: Return size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *next, *loop_thru_node;
+	size_t counter;
+	int loops = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loop_thru_node = examine_listint_loop(*h);
+
+	for (counter = 0; (*h != loop_thru_node || loops) && *h != NULL; *h = next)
+	{
+		counter++;
+		next = (*h)->next;
+
+		if (*h == loop_thru_node && loops)
+		{
+			if (loop_thru_node == loop_thru_node->next)
+			{
+				free(*h);
+				break;
+			}
+			counter++;
+			next = next->next;
+			free((*h)->next);
+			loops = 0;
+		}
+
+		free(*h);
+	}
+
+	*h = NULL;
+	return (counter);
+
+}
