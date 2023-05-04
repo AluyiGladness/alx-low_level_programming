32de266c2e3e7d786e50527be8c203d446b37f51
AluyiGladness <gangliahemit@gmail.com>
2023-05-05 00:00:43 +0100

    updated 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 #include <stdio.h>
-
+/**
+ * print_binary - prints binary format
+ * @n: parameter
+ * Return: nothing
+ */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n >> 1);
+	if (n > 1)
+		print_binary(n >> 1);
 
-    _putchar((n & 1) ? '1' : '0');
+	_putchar((n & 1) ? '1' : '0');
 }
 
